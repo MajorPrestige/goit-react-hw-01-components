1fd45dd0648cfc4be678e6b8d04ee698655cb5d4
Denis Dashkevich <majorprestige99@gmail.com>
2022-08-12 15:42:17 +0300

    finish

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,12 +1,35 @@
 import FriendListItem from './FriendListItem';
 import s from './FriendList.module.css';
+import PropTypes from 'prop-types';
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={s.friendlist}>
-      <FriendListItem friends={friends} />
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+          name={friend.name}
+        />
+      ))}
     </ul>
   );
 };
 
 export default FriendList;
+
+FriendList.defaultProps = {
+  friends: [],
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,34 +1,13 @@
 import s from './FriendList.module.css';
-import PropTypes from 'prop-types';
 
-const FriendListItem = ({ friends }) => {
-  return friends.map(friend => (
-    <li key={friend.id} className={s.item}>
-      <span className={friend.isOnline ? s.online : s.offline}></span>
-      <img
-        className={s.avatar}
-        src={friend.avatar}
-        alt="User avatar"
-        width="48"
-      />
-      <p className={s.name}>{friend.name}</p>
+const FriendListItem = ({ isOnline, avatar, name }) => {
+  return (
+    <li className={s.item}>
+      <span className={isOnline ? s.online : s.offline}></span>
+      <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={s.name}>{name}</p>
     </li>
-  ));
+  );
 };
 
 export default FriendListItem;
-
-FriendListItem.defaultProps = {
-  friends: [],
-};
-
-FriendListItem.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
-};
